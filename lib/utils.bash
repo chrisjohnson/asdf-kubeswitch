c3372278a2932a9a583e7b3342f9399a1bbc4e14
Chris Johnson <chrisjohnson0@gmail.com>
2026-03-25 11:36:27 -0400

    Update strategy to match kubeswitch downloaded files pattern

--- lib/utils.bash
+++ lib/utils.bash
@@ -70,9 +70,9 @@ install_version() {
 
 	(
 		mkdir -p "$install_path"
-		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path/switch"
+		chmod +x "$install_path/switch"
 
-		# TODO: Assert kubeswitch executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
